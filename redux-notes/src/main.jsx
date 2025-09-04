922881f18af7e9497d14a61e1c02390cdd9da60d
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-09-04 16:33:28 -0600

    fix: refactor the entire app using Redux Toolkit

--- redux-notes/src/main.jsx
+++ redux-notes/src/main.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
 import App from "./App.jsx";
-import noteReducer, { createNote } from "./reducers/noteReducer.js";
-import filterReducer, { filterChange } from "./reducers/filterReducer.js";
+import noteReducer from "./reducers/noteReducer.js";
+import filterReducer from "./reducers/filterReducer.js";
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
 
-const store = createStore(reducer);
-
 console.log(store.getState());
-
-store.subscribe(() => console.log(store.getState()));
-// store.dispatch(filterChange("IMPORTANT"));
-store.dispatch(
-  createNote("combineReducers forms one reducer from many simple reducers")
-);
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
--- redux-notes/src/reducers/filterReducer.js
+++ redux-notes/src/reducers/filterReducer.js
@@ -1,19 +1,17 @@
-const filterReducer = (state = "ALL", action) => {
-  console.log("ACTION: ", action);
-  switch (action.type) {
-    case "SET_FILTER":
-      return action.payload;
-    default:
-      return state;
-  }
-};
+import { createSlice } from "@reduxjs/toolkit";
 
-// action creators
-export const filterChange = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter,
-  };
-};
+const initialState = "ALL";
 
-export default filterReducer;
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+      const filter = action.payload;
+      return filter;
+    },
+  },
+});
+
+export const { filterChange } = filterSlice.actions;
+export default filterSlice.reducer;
