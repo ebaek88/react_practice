67b24a922ae56ccfbb3c1822ae3cf6b5939afbb3
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-09-16 16:51:31 -0600

    feat: test webpack bundling with simple React app

--- webpack-part7/src/App.js
+++ webpack-part7/src/App.js
@@ -1,7 +1,39 @@
-import React from "react"; // we need this now also in component files
+import React, { useState, useEffect } from "react"; // we need this now also in component files
+import axios from "axios";
+import PromisePolyfill from "promise-polyfill";
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill;
+}
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get(url).then((res) => setNotes(res.data));
+  }, [url]);
+
+  return notes;
+};
 
 const App = () => {
-  return <div>hello webpack</div>;
+  const [counter, setCounter] = useState(0);
+  const [values, setValues] = useState([]);
+  const notes = useNotes(BACKEND_URL);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    setValues(values.concat(counter));
+  };
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>press</button>
+      <div>
+        {notes.length} notes on server {BACKEND_URL}
+      </div>
+    </div>
+  );
 };
 
 export default App;
